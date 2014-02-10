edf98c8e135309e8c13b94f06a4f2a1fc196334d
Chris Granger <ibdknox@gmail.com>
2014-02-10 15:57:45 -0800

    0.0.4
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- emacs_compiled.js
+++ emacs_compiled.js
@@ -14,17 +14,14 @@ goog.require('lt.util.load');
 goog.require('lt.objs.editor');
 goog.require('lt.objs.editor.pool');
 goog.require('lt.objs.sidebar.command');
-
 lt.plugins.emacs.make_emacs_editor = (function make_emacs_editor(ed){lt.objs.editor.set_options.call(null,ed,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"keyMap","keyMap",4174180847),"emacs"], null));
 return lt.object.add_tags.call(null,ed,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"editor.keys.emacs","editor.keys.emacs",3283998978)], null));
 });
-
 lt.plugins.emacs.make_normal_editor = (function make_normal_editor(ed){lt.objs.editor.set_options.call(null,ed,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"keyMap","keyMap",4174180847),"default"], null));
 return lt.object.remove_tags.call(null,ed,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"editor.keys.emacs","editor.keys.emacs",3283998978)], null));
 });
-
-lt.objs.sidebar.command.command.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"emacs.keymap-cmd","emacs.keymap-cmd",1777328545),new cljs.core.Keyword(null,"desc","desc",1016984067),"Emacs: map to CodeMirror's emacs keymap",new cljs.core.Keyword(null,"hidden","hidden",4091384092),true,new cljs.core.Keyword(null,"exec","exec",1017031683),(function (keys,km){var temp__4092__auto__ = ((CodeMirror.keyMap[(function (){var or__5799__auto__ = km;if(cljs.core.truth_(or__5799__auto__))
-{return or__5799__auto__;
+lt.objs.sidebar.command.command.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"emacs.keymap-cmd","emacs.keymap-cmd",1777328545),new cljs.core.Keyword(null,"desc","desc",1016984067),"Emacs: map to CodeMirror's emacs keymap",new cljs.core.Keyword(null,"hidden","hidden",4091384092),true,new cljs.core.Keyword(null,"exec","exec",1017031683),(function (keys,km){var temp__4092__auto__ = ((CodeMirror.keyMap[(function (){var or__6757__auto__ = km;if(cljs.core.truth_(or__6757__auto__))
+{return or__6757__auto__;
 } else
 {return "emacs";
 }
@@ -38,7 +35,6 @@ lt.objs.sidebar.command.command.call(null,new cljs.core.PersistentArrayMap(null,
 {return null;
 }
 })], null));
-
 lt.plugins.emacs.__BEH__activate_emacs = (function __BEH__activate_emacs(this$){if(cljs.core.truth_(CodeMirror.keyMap.emacs))
 {} else
 {lt.util.load.js.call(null,"core/node_modules/codemirror/emacs.js",new cljs.core.Keyword(null,"sync","sync",1017449997));
@@ -50,7 +46,6 @@ if(cljs.core.truth_(lt.object.has_tag_QMARK_.call(null,this$,new cljs.core.Keywo
 }
 });
 lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.emacs","activate-emacs","lt.plugins.emacs/activate-emacs",2664292249),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.emacs.__BEH__activate_emacs,new cljs.core.Keyword(null,"desc","desc",1016984067),"Emacs: Activate Emacs mode",new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"object.instant","object.instant",773332388),null], null), null),new cljs.core.Keyword(null,"type","type",1017479852),new cljs.core.Keyword(null,"user","user",1017503549),new cljs.core.Keyword(null,"exclusive","exclusive",2700522000),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword("lt.plugins.vim","activate-vim","lt.plugins.vim/activate-vim",4199850093)], null));
-
 }
 
-//# sourceMappingURL=
+//# sourceMappingURL=emacs_compiled.js.map
--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/emacs "0.0.3"
+(defproject com.lighttable/emacs "0.0.4"
   :description "Emacs keybindings for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
